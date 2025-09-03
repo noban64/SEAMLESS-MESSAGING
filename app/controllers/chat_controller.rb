3e8c9567585bedb1737b02bad0fee0c0d8490216
Hamdi <32712674+noban64@users.noreply.github.com>
2025-09-03 14:01:16 +0100

    working chat system (bidoun websockets)

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,6 +1,15 @@
 class ChatController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :get_user_params, :verify_chat
 
+  def get_user_params
+    if params[:format]
+      @new_chat_user = User.find(params[:format])
+      @chat_one = Chat.where(first_user_id: current_user.id, second_user_id: @new_chat_user.id)
+      @chat_two = Chat.where(second_user_id: current_user.id, first_user_id: @new_chat_user.id)
+    end
+  end
+  def verify_chat
+  end
   def show
     if Chat.exists?(id: params[:id])
       @chat = Chat.find(params[:id])
@@ -9,34 +18,35 @@ class ChatController < ApplicationController
         redirect_to root_path
       end
     else
-      # create()
       flash[:notice] = "This chat doesn't exist."
       redirect_to root_path
     end
   end
 
   def new
-    @chat = Chat.new(first_user_id: current_user.id, second_user_id: params[:id])
+    @chat = Chat.new(first_user_id: current_user.id, second_user_id: @new_chat_user)
 
     render json: {}, status: 204
   end
 
   def create
-    @chat = Chat.new(first_user_id: current_user.id, second_user_id: params[:id])
-
-    begin
-      @chat.save
+    if Chat.exists?(@chat_one) || Chat.exists?(@chat_two)
+      @chat = Chat.find(@chat_one) || Chat.find(@chat_two)
+      puts ("Chat is already created. Redirecting to: " + @chat.id)
       redirect_to @chat
+    else
+      begin
+        @chat = Chat.new(first_user_id: current_user.id, second_user_id: @new_chat_user.id)
+        @chat.save
+        puts @chat.id
 
-    rescue => error
-      puts error
+        redirect_to chat_path(@chat)
+
+      rescue => error
+        puts error
+        flash[:notice] = "There was an error starting the chat. Please try again."
+        redirect_to root_path
+      end
     end
-
-    # if @chat.save then
-    #   redirect_to @chat
-    # else
-    #   flash[:notice] = "There was an error starting the chat. Please try again."
-    #   redirect_to root_path
-    # end
   end
 end
