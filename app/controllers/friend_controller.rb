6a84f484d9f429d0eaf614969565944c73b3fae1
Hamdi <32712674+noban64@users.noreply.github.com>
2025-09-10 16:01:31 +0100

    moved create to requests + working delete

--- app/controllers/friend_controller.rb
+++ app/controllers/friend_controller.rb
@@ -1,23 +1,41 @@
 class FriendController < ApplicationController
-  before_action :authenticate_user!, :get_user_params
+  before_action :authenticate_user!
+  # before_action :get_user_params, only: [ "create" ]
+  before_action :get_user_params, :find_friendship, only: [ "destroy" ]
 
   def get_user_params
     @requesting_user = current_user.id
-    @requested_user = params[:id]
+    @requested_user = params[:friend_data]
   end
-    def list
-      @friends_list = current_user.friends
-      @test_list = User.all()
-    end
 
-    def create
-      @friend = Friend.new(friend_id: @requested_user, user_id: @requesting_user)
-      ## must create unqiue index on friendlist
-      begin
-        @friend.save
-        redirect_to friends_list_path
-      rescue => error
-        puts error
-      end
-    end
+  def find_friendship
+    @friendship = Friend.where(user_id: current_user.id, friend_id: params[:friend_data])
+  end
+  def show
+    @friends_list = current_user.friendships
+    @test_list = User.all()
+  end
+
+  # def create
+  # MOVED TO REQUEST CONTROLLER TO AVOID COUNTLESS REDIRECTS
+  #     @friend = Friend.new(friend_id: @requested_user, user_id: @requesting_user)
+  #     ## must create unqiue index on friendlist
+  #     begin
+  #       @friend.save
+  #       redirect_to friends_list_path
+  #     rescue => error
+  #       puts error
+  #     end
+  # end
+
+  def destroy
+  begin
+    friendship = Friend.find(@friendship)
+    friendship.destroy
+  rescue => error
+    puts error
+  end
+  flash[:notice] = "Friend successfully deleted!"
+  redirect_to profile_path(@requested_user)
+  end
 end
