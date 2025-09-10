d82192296c3ea9692cbc95852112cb99168d009a
Hamdi <32712674+noban64@users.noreply.github.com>
2025-09-10 16:15:17 +0100

    table + indexing update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_28_115822) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_28_173529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -103,4 +103,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_28_115822) do
   add_foreign_key "messages", "chats"
   add_foreign_key "messages", "users"
   add_foreign_key "requests", "users"
+  add_foreign_key "requests", "users", column: "requesting_user_id"
 end
