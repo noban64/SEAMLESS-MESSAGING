cdeb1e31240e878e73de4a0cee3e7a3ed34da9fe
Hamdi <32712674+noban64@users.noreply.github.com>
2025-09-10 16:05:29 +0100

    temporary global param grabber

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def get_user_params_TEMP ## get this to work with all controllers that parameters
+    if !params[:id]
+      return "There is no profile selected!"
+    end
+    @requesting_user = current_user.id
+    @requested_user = params[:id]
+  end
 end
