a74942174f4cfca006c2ed3706ad92b9886d5717
Hamdi <32712674+noban64@users.noreply.github.com>
2025-09-10 16:02:15 +0100

    request buttons added + correctly determines relationship between users

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,21 +1,31 @@
 class ProfileController < ApplicationController
+  before_action :authenticate_user!
+  before_action :chat_exists?, :find_friendship, :request_exists?, only: [ "profile" ]
+
+  def chat_exists?
+    if Chat.exists?(first_user_id: current_user.id, second_user_id: params[:id])
+      @chat = Chat.where(first_user_id: current_user.id, second_user_id: params[:id])
+      @returned_chat = Chat.find(@chat)
+    elsif Chat.exists?(second_user_id: current_user.id, first_user_id: params[:id])
+      @chat = Chat.where(second_user_id: current_user.id, first_user_id: params[:id])
+      @returned_chat = Chat.find(@chat)
+    else
+      @returned_chat = nil
+    end
+  end
+  def request_exists?
+    @request_exists  = Request.exists?(requesting_user_id: @current_user.id, user_id: params[:id], denied: false)
+  end
+
+  def find_friendship
+    @already_friends  = Friend.exists?(friend_id: @current_user.id, user_id: params[:id])
+  end
   def profile
       if User.exists?(id: params[:id]) then #### previous sqli issue, make sure to test it further.
           @profile = User.find(params[:id])
       else
         flash[:notice] = "That user does not exist!"
         redirect_to root_path
-      end
-
-      if Chat.exists?(first_user_id: current_user.id, second_user_id: params[:id])
-        @chat = Chat.where(first_user_id: current_user.id, second_user_id: params[:id])
-        @returned_chat = Chat.find(@chat)
-      elsif Chat.exists?(second_user_id: current_user.id, first_user_id: params[:id])
-        @chat = Chat.where(second_user_id: current_user.id, first_user_id: params[:id])
-        @returned_chat = Chat.find(@chat)
-      else
-        @returned_chat = nil
-
       end
   end
 end
